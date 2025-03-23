f85f576e109f05f7e12919f6d4ab8579e595fcfe
Patrick Pailhes <193191145+patrick498@users.noreply.github.com>
2025-03-23 15:20:21 +0900

    Updated navbar

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,17 @@
 // This is where it all goes :)
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Highlight button clicked on navbar
+  document.querySelectorAll('.nav-link').forEach(link => {
+    console.log('Hello');
+
+    link.addEventListener('click', (event) => {
+      // Remove active class from all navbar buttons
+      document.querySelectorAll('.nav-link').forEach(link => {
+        link.classList.remove('active');
+      });
+      // Add active class to clicked button
+      event.currentTarget.classList.add('active');
+    });
+  });
+});
